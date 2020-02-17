090734366cb1d340e22b5ab6b2fa456ea5db69b6
fwcd <fwcdmail@gmail.com>
2020-02-17 14:00:02 +0100

    Implement apply for substitutions

--- src/Subst.hs
+++ src/Subst.hs
@@ -1,6 +1,9 @@
 module Subst where
 
+import Type
+
 data Subst = Subst [(VarName, Term)]
+    deriving Show
 
 -- An empty substitution.
 empty :: Subst
@@ -9,3 +12,8 @@ empty = Subst []
 -- Creates a substitution that maps a single variable to a term.
 single :: VarName -> Term -> Subst
 single v t = Subst [(v, t)]
+
+-- Applies a substitution to a term.
+apply :: Subst -> Term -> Term
+apply (Subst s) (Var v) = maybe (Var v) id $ lookup v s
+apply subst (Comb name ts) = Comb name $ map (apply subst) ts
