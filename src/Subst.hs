2dd9116ab021e44370b1b04b7754a02cb54fa248
fwcd <fwcdmail@gmail.com>
2020-02-17 13:41:40 +0100

    Add substitutions module

--- src/Subst.hs
+++ src/Subst.hs
@@ -1,1 +1,11 @@
 module Subst where
+
+data Subst = Subst [(VarName, Term)]
+
+-- An empty substitution.
+empty :: Subst
+empty = Subst []
+
+-- Creates a substitution that maps a single variable to a term.
+single :: VarName -> Term -> Subst
+single v t = Subst [(v, t)]
