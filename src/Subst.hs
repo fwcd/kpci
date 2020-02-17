0d08de615a7680c350ae55c02eb2068838de7c6c
fwcd <fwcdmail@gmail.com>
2020-02-17 14:39:36 +0100

    Implement restrictTo for substitutions

--- src/Subst.hs
+++ src/Subst.hs
@@ -20,6 +20,10 @@ apply :: Subst -> Term -> Term
 apply (Subst s) (Var v) = maybe (Var v) id $ lookup v s
 apply subst (Comb name ts) = Comb name $ map (apply subst) ts
 
+-- Restricts a substitution to the given variable names.
+restrictTo :: [VarName] -> Subst -> Subst
+restrictTo vs (Subst s) = Subst $ filter (flip elem vs . fst) s
+
 -- Creates a substitution by first applying the right and then the left one.
 compose :: Subst -> Subst -> Subst
 compose (Subst s2) (Subst s1) = Subst $ map (\(v, t) -> (v, apply (Subst s2) t)) s1 ++ s2
