6c3c72996ec30db5deeb6b20b63a4ce5bdfddc61
fwcd <fwcdmail@gmail.com>
2020-02-20 14:54:37 +0100

    Improve renaming scheme
    Add uniquing numbers to variable identifiers.

--- src/Rename.hs
+++ src/Rename.hs
@@ -9,33 +9,46 @@ import Vars
 -- that occur in the given list.
 rename :: [VarName] -> Rule -> (Rule, [VarName])
 rename used r = (r', used')
-    where (r', (_, used')) = runState (renameInRule r) (filter (not . flip elem used) $ freshVars, used)
+    where (r', (_, used')) = runState (renameInRule r) (1, used)
 
 -- Renames variables using unique variable names in a rule.
-renameInRule :: Rule -> State ([VarName], [VarName]) Rule
+renameInRule :: Rule -> State (Int, [VarName]) Rule
 renameInRule (Rule t ts) = do
-    (fresh, used) <- get
-
     let vs = allVars t ++ (ts >>= allVars)
-        (fs, fs') = splitAt (length vs) fresh
-        s = Subst $ zip vs $ map Var fs
-    put (fs', used ++ fs)
+    vs' <- sequence $ map renameVarName vs
 
+    let s = Subst $ zip vs $ map Var vs'
     t' <- renameApply s t
     ts' <- sequence $ map (renameApply s) ts
 
     return $ Rule t' ts'
 
 -- Applies a substitution and renames anonymous variables in a term.
-renameApply :: Subst -> Term -> State ([VarName], [VarName]) Term
+renameApply :: Subst -> Term -> State (Int, [VarName]) Term
 renameApply s t = do
     t' <- renameAnonymous t
     return $ apply s t'
 
 -- Renames all anonymous variables in a term.
-renameAnonymous :: Term -> State ([VarName], [VarName]) Term
-renameAnonymous (Var x) | x == "_"  = do (fresh, used) <- get
-                                         put (tail fresh, head(fresh):used)
-                                         return $ Var $ head fresh
-                        | otherwise = return $ Var x
+renameAnonymous :: Term -> State (Int, [VarName]) Term
+renameAnonymous (Var s) | s == "_"  = fmap Var $ renameVarName s
+                        | otherwise = return $ Var s
 renameAnonymous (Comb f ts) = fmap (Comb f) $ sequence $ map renameAnonymous ts
+
+-- Makes a variable name unique by appending a number.
+renameVarName :: VarName -> State (Int, [VarName]) VarName
+renameVarName s = do
+  (n, used) <- get
+  let newS = renameVarName' s n used
+  put (n, newS : used)
+  return newS
+  where renameVarName' :: VarName -> Int -> [VarName] -> VarName
+        renameVarName' s' i used' | elem s'' used' = renameVarName' s' (i + 1) used'
+                                  | otherwise      = s''
+          where s'' = s' ++ show i
+
+-- The beginning of the list and the longest suffix
+-- of elements satisfying the predicate.
+spanEnd :: (a -> Bool) -> [a] -> ([a], [a])
+spanEnd f xs = reverseResult $ span f $ reverse xs
+  where reverseResult (p, s) = (reverse s, reverse p)
