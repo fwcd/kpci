af5ba6745a865820b15afd20134b1370ca681037
fwcd <fwcdmail@gmail.com>
2020-02-20 15:04:16 +0100

    Implement :r REPL command for reloading

--- src/REPL.hs
+++ src/REPL.hs
@@ -7,12 +7,13 @@ import System.Exit
 import System.IO (hFlush, stdout)
 import Type
 
-data REPLState = REPLState Prog Strategy
+-- Holds state used by the interactive shell, e.g. the loaded program and the SLD resolution strategy.
+data REPLState = REPLState Prog (Maybe FilePath) Strategy
 
 -- Runs an interactive Prolog shell.
 runREPL :: IO ()
 runREPL = repl (Just "Welcome!\nType \":h\" for help.") st
-  where st = REPLState (Prog []) (const [])
+  where st = REPLState (Prog []) Nothing (const [])
 
 -- Runs the REPL loop.
 repl :: Maybe String -> REPLState -> IO ()
@@ -39,22 +40,34 @@ evaluate input st = case input of
     case parse input of
       Left e -> putStrLn e
       Right g -> putStrLn $ pretty $ sld p g
-        where (REPLState p _) = st
+        where (REPLState p _ _) = st
     return st
 
 -- Evaluates a command prefixed with ':'.
 evaluateCommand :: String -> REPLState -> IO REPLState
 evaluateCommand cmd st = case cmd of
   -- TODO: Implement :h and others
-  'l' : path -> do
-    f <- parseFile path
-    case f of
-      Left e -> do
-        putStrLn e
+  'l' : path -> loadFile path st
+  "r" -> case p of
+      Just path -> loadFile path st
+      Nothing -> do
+        putStrLn "No file loaded yet!"
         return st
-      Right p -> return $ REPLState p strat
-        where (REPLState _ strat) = st
+    where (REPLState _ p _) = st
   "q" -> do
     putStrLn "Goodbye!"
     exitSuccess
   _   -> return st
+
+-- Loads a file into the interpreter.
+loadFile :: FilePath -> REPLState -> IO REPLState
+loadFile path st = do
+  f <- parseFile path
+  case f of
+    Left e -> do
+      putStrLn e
+      return st
+    Right p -> do
+      putStrLn "Success!"
+      return $ REPLState p (Just path) strat
+      where (REPLState _ _ strat) = st
