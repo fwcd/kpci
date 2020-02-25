4a01a3331b9da2a2e329f3406b289c5f55150eef
fwcd <fwcdmail@gmail.com>
2020-02-25 15:32:18 +0100

    Implement IDDFS

--- src/SLD.hs
+++ src/SLD.hs
@@ -34,17 +34,26 @@ instance Pretty SLDTree where
           prettyChild (s, t) = (arr ++ pretty s) : (map ((flip replicate ' ' $ length arr) ++) $ pretty' t)
             where arr = "=> "
 
+-- Finds the height of the tree.
+height :: SLDTree -> Int
+height (SLDTree _ []) = 1
+height (SLDTree _ cs) = 1 + (foldr max 0 $ height <$> snd <$> cs)
+
 -- Performs a depth-first search on the SLD tree.
+-- The central idea of the algorithm is to recursively
+-- carry an accumulated substitution around.
 dfs :: Strategy
 dfs = dfs' empty
   where dfs' :: Subst -> Strategy
-        dfs' s (SLDTree (Goal [])    []) = [s]
-        dfs' _ (SLDTree (Goal (_:_)) []) = []
-        dfs' s (SLDTree _ cs) = cs >>= (dfsChild s)
+        dfs' s (SLDTree (Goal [])    []) = [s] -- Successful leaf node
+        dfs' _ (SLDTree (Goal (_:_)) []) = []  -- Failure leaf node
+        dfs' s (SLDTree _ cs)            = cs >>= (dfsChild s)
         dfsChild :: Subst -> (Subst, SLDTree) -> [Subst]
         dfsChild s1 (s2, t) = dfs' (compose s2 s1) t
 
--- Performs a breadth-first search on the SLD tree
+-- Performs a breadth-first search on the SLD tree.
+-- The central idea of the algorithm is to carry an
+-- accumulated substitution in the BFS queue.
 bfs :: Strategy
 bfs t = bfs' [(empty,t)]
   where bfs' :: [(Subst, SLDTree)] -> [Subst]
@@ -57,12 +66,25 @@ bfs t = bfs' [(empty,t)]
         composeMap _ []                 = []
         composeMap s ((subst, tree):ts) = (compose subst s, tree): composeMap s ts
 
+-- Performs a depth-first search that only finds the leaves at the specified depth.
+dfsN :: Int -> Strategy
+dfsN = flip dfsN' empty
+  where dfsN' :: Int -> Subst -> Strategy
+        dfsN' n s (SLDTree (Goal [])    []) | n == 0    = [s]
+                                            | otherwise = []
+        dfsN' _ _ (SLDTree (Goal (_:_)) [])             = []
+        dfsN' n s (SLDTree _ cs)            | n <= 0    = []
+                                            | otherwise = cs >>= (dfsChildN n s)
+        dfsChildN :: Int -> Subst -> (Subst, SLDTree) -> [Subst]
+        dfsChildN n s1 (s2, t) = dfsN' (n - 1) (compose s2 s1) t
 
-
+-- Performs an iteratively deepening depth-first search on the SLD tree
+iddfs :: Strategy
+iddfs t = [0..height t] >>= flip dfsN t
 
 -- Maps strategy names to strategies.
 strategies :: [(String, Strategy)]
-strategies = [("dfs", dfs), ("bfs", bfs)]
+strategies = [("dfs", dfs), ("bfs", bfs), ("iddfs", iddfs)]
 
 -- A default strategy.
 defaultStrategy :: Strategy
