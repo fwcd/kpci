02ea4408a61de3e33a13c1b491d538e8580e89d2
fwcd <fwcdmail@gmail.com>
2020-02-25 13:30:46 +0100

    Setup Cabal for testing

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,13 +1,19 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 import Test.QuickCheck
+import System.Exit
 
 prop_simple :: Int -> Bool
-prop_simple x = x == x
+prop_simple x = 1 == 2
+
+prop_test :: Bool
+prop_test = 1 == 4
 
 return []
 
 main :: IO ()
 main = do
   result <- $quickCheckAll
-  putStrLn $ show result
+  if result
+    then exitSuccess
+    else exitFailure
