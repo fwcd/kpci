52f56746cc56af766410f4abaa4e96451e01c139
fwcd <fwcdmail@gmail.com>
2020-02-26 16:12:42 +0100

    Test partial evaluation

--- examples/tests/map.pl
+++ examples/tests/map.pl
@@ -1,3 +1,10 @@
+% Test map predicate
 map(not, [], []).
 map(not, [true, true, false], [false, false, true]).
 map(constThree, [true, false, 9, 3, false, true], [3, 3, 3, 3, 3, 3]).
+
+% Test partial evaluation
+call(map, not, [true, true], [false, false]).
+call(map(not), [true, true], [false, false]).
+call(map(not, [true, true]), [false, false]).
+call(map(not, [true, true], [false, false])).
