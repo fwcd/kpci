6fc28b013387e720c03e0a30f5d3d86d866a44bc
fwcd <fwcdmail@gmail.com>
2020-02-26 15:02:20 +0100

    Add more 'map' tests

--- examples/rules/map.pl
+++ examples/rules/map.pl
@@ -1,5 +1,8 @@
-not(true, false).
-not(false, true).
-
+% Maps a predicate over a list.
 map(_, [], []).
 map(P, [E|L], [F|M]) :- call(P, E, F), map(P, L, M).
+
+% Utility predicates, useful for testing map.
+not(true, false).
+not(false, true).
+constThree(X, 3).
--- examples/tests/map.pl
+++ examples/tests/map.pl
@@ -1,1 +1,3 @@
+map(not, [], []).
 map(not, [true, true, false], [false, false, true]).
+map(constThree, [true, false, 9, 3, false, true], [3, 3, 3, 3, 3, 3]).
